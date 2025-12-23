0b1710a785348f34ff4099b98668394e9dc12441
Abhishek010-5 <0105ds211002@gmail.com>
2025-12-23 20:54:35 +0530

    added logger to display errors

--- app/database.py
+++ app/database.py
@@ -1,12 +1,14 @@
 from pymongo import MongoClient
 from app.config import settings
+import logging
 
+logger = logging.getLogger(__name__)
 def get_db():
     try:
         client = MongoClient(settings.database_url)
         db = client[settings.database_name]
         return db
     except Exception as e:
-        print(e)
+        logger.error(str(e))
     
     client.close()
