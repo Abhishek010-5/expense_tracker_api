fa4b65f983cf05fd97b4d0765e1512d9ebab5b14
Abhishek010-5 <0105ds211002@gmail.com>
2025-12-27 20:27:14 +0530

    update

--- app/main.py
+++ app/main.py
@@ -44,16 +44,9 @@ def create_app():
     CORS(
         app,
         supports_credentials=True,
-        origins=[
-            "http://localhost:3000",
-            "http://127.0.0.1:5500",
-            "http://localhost:5500",
-            "http://localhost:8000",
-        ],
-        allow_headers=["Content-Type", "X-API-Key", "Authorization"],
-        expose_headers=["Content-Type", "Authorization"],
-        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-    )
+        origins="*",
+        allow_headers=["Content-Type", "X-API-Key", "Authorization"]
+        )
     # Bind the limiter to the app AFTER creation
     limiter.init_app(app)
 
