8d7e844cc79a896283e76fb993a1f83af9bd12e7
Abhishek010-5 <0105ds211002@gmail.com>
2026-01-17 10:22:08 +0530

    chore: remove unused commented-out code

--- app/database.py
+++ app/database.py
@@ -1,18 +1,3 @@
-# from pymongo import MongoClient
-# from app.config import settings
-# import logging
-
-# logger = logging.getLogger(__name__)
-# def get_db():
-#     try:
-#         client = MongoClient(settings.database_url)
-#         db = client[settings.database_name]
-#         return db
-#     except Exception as e:
-#         logger.error(str(e))
-#     finally:
-#         client.close()
-        
 from pymongo import MongoClient
 from app.config import settings
 import logging
